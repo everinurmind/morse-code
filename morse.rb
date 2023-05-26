f48732720a4b444dc9db64c50538fc4d6683070d
everinurmind <nurbol.sultanov@gmail.com>
2023-05-25 23:32:11 -0600

    Fix rubocop linters

--- morse.rb
+++ morse.rb
@@ -1,21 +1,23 @@
 def decode_char(code)
-    morse_code = {
-      ".-" => "A", "-..." => "B", "-.-." => "C", "-.." => "D", "." => "E",
-      "..-." => "F", "--." => "G", "...." => "H", ".." => "I", ".---" => "J",
-      "-.-" => "K", ".-.." => "L", "--" => "M", "-." => "N", "---" => "O",
-      ".--." => "P", "--.-" => "Q", ".-." => "R", "..." => "S", "-" => "T",
-      "..-" => "U", "...-" => "V", ".--" => "W", "-..-" => "X", "-.--" => "Y",
-      "--.." => "Z"
-    }
-    morse_code[code]
-  end
-  def decode_word(word)
-    word.split(" ").map { |char| decode_char(char) }.join
-  end
-  def decode(message)
-    message.split("   ").map { |word| decode_word(word) }.join(" ")
-  end
-  # Decode of the message from the old bottle:
-  morse_code = ".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ..."
-  decoded_message = decode(morse_code)
-  puts decoded_message
+  morse_code = {
+    ".-" => "A", "-..." => "B", "-.-." => "C", "-.." => "D", "." => "E",
+    "..-." => "F", "--." => "G", "...." => "H", ".." => "I", ".---" => "J",
+    "-.-" => "K", ".-.." => "L", "--" => "M", "-." => "N", "---" => "O",
+    ".--." => "P", "--.-" => "Q", ".-." => "R", "..." => "S", "-" => "T",
+    "..-" => "U", "...-" => "V", ".--" => "W", "-..-" => "X", "-.--" => "Y",
+    "--.." => "Z"
+  }
+  morse_code[code]
+end
+
+def decode_word(word)
+  word.split(" ").map { |char| decode_char(char) }.join
+end
+
+def decode(message)
+  message.split("   ").map { |word| decode_word(word) }.join(" ")
+end
+# Decode of the message from the old bottle:
+morse_code = ".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ..."
+decoded_message = decode(morse_code)
+puts decoded_message
